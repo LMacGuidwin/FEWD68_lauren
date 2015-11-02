c684bb9e0bb04520c321bb4b0dfe1ef42f283a2f
LMacGuidwin <laumacgu@usnycrad2510.global.publicisgroupe.net>
2015-11-02 15:21:43 -0800

    Relaxr Landing 2

--- Final Project/CODE/js/javascript.js
+++ Final Project/CODE/js/javascript.js
@@ -1,15 +1,9 @@
 
 
-$(window).scroll(
-    {
-        previousTop: 0
-    }, 
-    function () {
-    var currentTop = $(window).scrollTop();
-    if (currentTop < this.previousTop) {
-        $(".nav").show();
-    } else {
-        $(".nav").hide();
-    }
-    this.previousTop = currentTop;
-});
+var main = function(){
+    $(".doneFilter").click(function(){
+    $("div.notDone").addClass("invisible");
+    });
+};
+
+(document).ready(main);
